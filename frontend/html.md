348cf32ec02746d76d4bec60646ad22382d2618d
yaow <1753552156@qq.com>
2024-08-16 22:16:05 +0800

    feat: add promise.all

---
+++ frontend/html.md
@@ -0,0 +1,33 @@
+#### 手写 Promise.All
+
+```js
+Promise.myAll = function (proms) {
+  let res;
+  let rej;
+  const p = new Promise((resolve, reject) => {
+    res = resolve;
+    rej = reject;
+  });
+  let i = 0;
+  const result = [];
+  for (const prom of proms) {
+    const index = i;
+    i++;
+    Promise.resolve(prom).then((data) => {
+      //1.将完成的数据加入到最终结果
+      result[index] = data;
+
+      //2.判断是否全部完成
+      i--;
+      if (i === 0) {
+        //全部完成
+        res(result);
+      }
+    }, rej);
+  }
+  if (i === 0) {
+    res([]);
+  }
+  return p;
+};
+```
---
+++ frontend/utils/promiseAll.js
@@ -0,0 +1,40 @@
+//@ts-nocheck
+Promise.myAll = function (proms) {
+  let res;
+  let rej;
+  const p = new Promise((resolve, reject) => {
+    res = resolve;
+    rej = reject;
+  });
+  let i = 0;
+  const result = [];
+  for (const prom of proms) {
+    const index = i;
+    i++;
+    Promise.resolve(prom).then((data) => {
+      //1.将完成的数据加入到最终结果
+      result[index] = data;
+
+      //2.判断是否全部完成
+      i--;
+      if (i === 0) {
+        //全部完成
+        res(result);
+      }
+    }, rej);
+  }
+  if (i === 0) {
+    res([]);
+  }
+  return p;
+};
+
+Promise.myAll([1, 2, 3, Promise.reject(1), Promise.reject(2)]).then(
+  (res) => {
+    console.log("res", res);
+  },
+  (rej) => {
+    console.log("rej", rej);
+  }
+);
+console.log();
