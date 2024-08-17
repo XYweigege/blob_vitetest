a041abe4b4b80d1d4421f4bf446dbf43298f3afd
yaow <1753552156@qq.com>
2024-08-17 11:41:38 +0800

    11

--- .vitepress/sidebar.js
+++ .vitepress/sidebar.js
@@ -12,9 +12,18 @@ module.exports = {
           items: [
             { text: "基础", link: "/frontend/html/basics" },
             { text: "进阶", link: "/frontend/html/advanced" },
+            { text: "手写", link: "/frontend/html/shouxie" },
           ],
         },
-        { text: "CSS", link: "/frontend/css" },
+        {
+          text: "CSS",
+          link: "/frontend/css/index",
+          items: [
+            { text: "基础", link: "/frontend/css/basics" },
+            { text: "进阶", link: "/frontend/css/advanced" },
+            { text: "手写", link: "/frontend/css/shouxie" },
+          ],
+        },
         { text: "JavaScript", link: "/frontend/javascript" },
       ],
     },
