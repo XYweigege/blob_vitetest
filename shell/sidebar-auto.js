56a706af1a0e35977b7305df6ad896c1d30288f7
yaow <1753552156@qq.com>
2024-08-17 11:22:53 +0800

    q1

--- shell/sidebar-auto.js
+++ shell/sidebar-auto.js
@@ -9,4 +9,14 @@ module.exports = {
       link: "/frontend/index",
     },
   ],
+  "/backend/": [
+    {
+      text: "后端",
+      items: [
+        { text: "介绍", link: "/backend/" },
+        { text: "Node.js", link: "/backend/nodejs" },
+        { text: "数据库", link: "/backend/database" },
+      ],
+    },
+  ],
 };
