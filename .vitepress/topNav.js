10899a1b6afa317550937ee78e4b08e0bb0d3fe0
yaow <1753552156@qq.com>
2024-08-26 22:15:06 +0800

    11

--- .vitepress/topNav.js
+++ .vitepress/topNav.js
@@ -8,10 +8,10 @@ export default [
     text: "前端网聚",
     items: [{ text: "前端综合", link: "/site/page" }],
   },
-  {
-    text: "技术笔记",
-    items: [{ text: "VitePress", link: "/note/vitePress" }],
-  },
+  // {
+  //   text: "技术笔记",
+  //   items: [{ text: "VitePress", link: "/note/vitePress" }],
+  // },
   {
     text: "关于我们",
     link: "/about/page",
