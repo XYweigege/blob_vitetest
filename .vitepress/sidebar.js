012f3e5b434fad74f83600d62731f8b37cea8101
weigedediannao <1753552156@qq.com>
2025-03-30 17:31:30 +0800

    feat: add advace

--- .vitepress/sidebar.js
+++ .vitepress/sidebar.js
@@ -6,6 +6,11 @@ module.exports = {
       text: "前端",
       items: [
         { text: "介绍", link: "/frontend/" },
+        { text: "高级的", link: "/frontend/",
+          items: [
+            { text: "基础", link: "/frontend/advance/basic" },
+           ],
+         },
         {
           text: "HTML",
           link: "/frontend/html",
